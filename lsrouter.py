33f50c2b8cb3f0fcc58a97f36c9ed3483072e750
= <leonard.debroux@gmail.com>
2012-11-29 12:06:32 +0100

    Type and etc

--- lsrouter.py
+++ lsrouter.py
@@ -1,12 +1,21 @@
-
+import socket
+from Type import *
 
 class LsRouter:
 
+    self.s = socket.socketpair(socket.AF_INET, socket.SOCK_DGRAM)
 
-
-
-
-
+    def send(self, type, sender, reveiver, message = None):
+        if type == Type.HELLO:
+            pass
+        elif type == Type.LSP:
+            pass
+        elif type == Type.LSACK:
+            pass
+        elif type == Type.DATA:
+            pass
+        else:
+            print("Wrong Type")
 
 
 
