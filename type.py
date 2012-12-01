cc190e9c1c8135644cbf02804187077dd930b24c
Ben <dbapps2@gmail.com>
2012-12-01 12:57:43 +0100

    Update type.py

--- type.py
+++ type.py
@@ -1,13 +1,8 @@
-'''
-Created on Nov 29, 2012
-
-@author: inekar
-'''
 class Type:
     ''' 
-    Enumatation of the different types of message
+    Enumeration of the different types of message
     '''
     HELLO = 0
-    LSP = 1
+    LSP   = 1
     LSACK = 2
-    DATA = 3
+    DATA  = 3
